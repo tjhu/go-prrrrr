922560ecd8feec14bbcdc964e9fecb1edb81120d
Tianjiao Huang <git@tjhu.dev>
2022-06-07 18:14:57 -0700

    chain filter tests

--- stream/filter_test.go
+++ stream/filter_test.go
@@ -7,21 +7,56 @@ import (
 )
 
 func TestFilter(t *testing.T) {
-	slice := []int{1, 2, 3, 4, 10, 100}
+	greater_than_ten := func(x int) bool { return x > 10 }
+	less_than_ten := func(x int) bool { return x < 10 }
+	even := func(x int) bool { return x%2 == 0 }
+	odd := func(x int) bool { return x%2 != 0 }
 
 	// Greater than 10
-	greater_than_ten := func(x int) bool { return x > 10 }
-	assert.ElementsMatch(t, []int{100}, OfSlice(slice).Filter(greater_than_ten).ToSlice())
+	t.Run("Greater", func(t *testing.T) {
+		slice := []int{1, 2, 3, 4, 10, 100}
+		assert.ElementsMatch(t, []int{100}, OfSlice(slice).Filter(greater_than_ten).ToSlice())
+	})
 
 	// Less than 10
-	less_than_ten := func(x int) bool { return x < 10 }
-	assert.ElementsMatch(t, []int{1, 2, 3, 4}, OfSlice(slice).Filter(less_than_ten).ToSlice())
+	t.Run("Less", func(t *testing.T) {
+		slice := []int{1, 2, 3, 4, 10, 100}
+		assert.ElementsMatch(t, []int{1, 2, 3, 4}, OfSlice(slice).Filter(less_than_ten).ToSlice())
+	})
 
 	// Even
-	even := func(x int) bool { return x%2 == 0 }
-	assert.ElementsMatch(t, []int{2, 4, 10, 100}, OfSlice(slice).Filter(even).ToSlice())
+	t.Run("Even", func(t *testing.T) {
+		slice := []int{1, 2, 3, 4, 10, 100}
+		assert.ElementsMatch(t, []int{2, 4, 10, 100}, OfSlice(slice).Filter(even).ToSlice())
+	})
 
 	// Odd
-	odd := func(x int) bool { return x%2 != 0 }
-	assert.ElementsMatch(t, []int{1, 3}, OfSlice(slice).Filter(odd).ToSlice())
+	t.Run("Odd", func(t *testing.T) {
+		slice := []int{1, 2, 3, 4, 10, 100}
+		assert.ElementsMatch(t, []int{1, 3}, OfSlice(slice).Filter(odd).ToSlice())
+	})
+
+	// Greater and less than 10
+	t.Run("GreaterLess", func(t *testing.T) {
+		slice := []int{1, 2, 3, 4, 10, 100}
+		assert.ElementsMatch(t, []int{}, OfSlice(slice).Filter(greater_than_ten).Filter(less_than_ten).ToSlice())
+	})
+
+	// Even and odd
+	t.Run("EvenOdd", func(t *testing.T) {
+		slice := []int{1, 2, 3, 4, 10, 100}
+		assert.ElementsMatch(t, []int{}, OfSlice(slice).Filter(even).Filter(odd).ToSlice())
+	})
+
+	// Even less than 10
+	t.Run("EvenLess", func(t *testing.T) {
+		slice := []int{1, 2, 3, 4, 10, 100}
+		assert.ElementsMatch(t, []int{2, 4}, OfSlice(slice).Filter(even).Filter(less_than_ten).ToSlice())
+	})
+
+	// Greater than 10 even
+	t.Run("GreaterEven", func(t *testing.T) {
+		slice := []int{12, 1, 2, 3, 4, 10, 100}
+		assert.ElementsMatch(t, []int{12, 100}, OfSlice(slice).Filter(greater_than_ten).Filter(even).ToSlice())
+	})
 }
